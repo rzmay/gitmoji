388cac320aa13e161e5002adbf9685544ab9f3c2
rzmay <robertmay2003@gmail.com>
2021-07-06 01:44:28 -0700

    🦦 Async

--- src/index.js
+++ src/index.js
@@ -3,15 +3,14 @@ const weights = require('./weights.json');
 
 const wordpos = new WordPOS();
 
-function generate(message) {
-  wordpos.getPOS(message, (pos) => {
-    const words = Object.keys(pos).map((key) => (
-      pos[key].map((word) => ({
-        word,
-        weight: weights[key],
-      }))
-    )).flat();
+async function generate(message) {
+  const pos = await wordpos.getPOS(message);
+  const words = Object.keys(pos).map((key) => (
+    pos[key].map((word) => ({
+      word,
+      weight: weights[key],
+    }))
+  )).flat();
 
-    // Add emojis to words, multiply weight by score, return emoji with highest weight
-  });
+  // Add emojis to words, multiply weight by score, return emoji with highest weight
 }

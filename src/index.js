499c1e340c356873474c3b881e996635f1b45db9
rzmay <robertmay2003@gmail.com>
2021-07-06 02:38:23 -0700

    🎭 Added sentiment fallback & forbidden

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 const WordPOS = require('wordpos');
 const emojiFromWord = require('emoji-from-word');
+const Sentiment = require('sentiment');
 const weights = require('./weights.json');
+const { forbidden } = require('./forbidden.json');
+const { emojis: sentimentEmojis } = require('./sentimentEmojis.json');
 
+const sentiment = new Sentiment();
 const wordpos = new WordPOS();
 
 module.exports = async function generate(message) {
@@ -13,12 +17,25 @@ module.exports = async function generate(message) {
     }))
   )).flat();
 
-  return words.map(({ word, weight }) => {
-    const { emoji, score } = emojiFromWord(word);
+  let bestMatch = words.map(({ word, weight }) => {
+    // eslint-disable-next-line camelcase
+    const { emoji, emoji_name, score } = emojiFromWord(word);
+    console.log(word, emoji, score);
+
+    if (!emoji || forbidden.includes(emoji_name)) return { score: 0 };
 
     return {
       emoji: emoji.char,
       score: weight * score,
     };
   }).reduce((a, b) => (a.score > b.score ? a : b));
+
+  if (bestMatch.score < weights.sentiment) {
+    const { comparative } = sentiment.analyze(message);
+    bestMatch = sentimentEmojis
+      .filter(({ threshold }) => (comparative + 1) / 2 > threshold)
+      .reduce((a, b) => (a.threshold > b.threshold ? a : b));
+  }
+
+  return `${bestMatch.emoji} ${message}`;
 };

87e0c01cc212c934de55609c4e52c9d2d07880aa
rzmay <robertmay2003@gmail.com>
2021-07-06 01:41:52 -0700

    🩰 Extract weights, array generation elegance

--- src/index.js
+++ src/index.js
@@ -1,31 +1,16 @@
 const WordPOS = require('wordpos');
+const weights = require('./weights.json');
 
 const wordpos = new WordPOS();
 
 function generate(message) {
   wordpos.getPOS(message, (pos) => {
-    const words = [
-      ...(pos.nouns.map((word) => ({
+    const words = Object.keys(pos).map((key) => (
+      pos[key].map((word) => ({
         word,
-        weight: 1.0,
-      }))),
-      ...(pos.verbs.map((word) => ({
-        word,
-        weight: 0.85,
-      }))),
-      ...(pos.adjectives.map((word) => ({
-        word,
-        weight: 0.75,
-      }))),
-      ...(pos.adverbs.map((word) => ({
-        word,
-        weight: 0.65,
-      }))),
-      ...(pos.rest.map((word) => ({
-        word,
-        weight: 0.5,
-      }))),
-    ];
+        weight: weights[key],
+      }))
+    )).flat();
 
     // Add emojis to words, multiply weight by score, return emoji with highest weight
   });
